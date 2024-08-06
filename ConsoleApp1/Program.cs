cdcbc512f17f0fc7eae9ec82a97a320e188661b9
USER <USER@DESKTOP-IJ48L0U>
2024-08-06 13:28:29 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("silahkan masukan detik");
+            double angka1 = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+         double hasil = angka1 / 60;
+         double hasil2 = angka1 / 3600;
+
+            Console.WriteLine(hasil);
+            Console.WriteLine(hasil2);
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int angka1, hasil;
+            Console.WriteLine("silahkan masukan nama");
+            string nama = Console.ReadLine();
+            Console.WriteLine("silahkan masukan tahun lahir");
+            angka1 = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+
+            hasil = 2024 - angka1;
+
+            Console.WriteLine("umur :" + hasil);
+            Console.WriteLine("nama :" + nama);
+        }
+    }
+}
